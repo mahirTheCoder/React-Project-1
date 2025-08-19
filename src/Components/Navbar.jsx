e69613a9fb434c84525bb94bc2077dc7f55a277d
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-08-19 21:14:49 +0600

    nav and nav items add

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
+import { Link } from 'react-router'
 const Navbar = () => {
+const navItems = [
+  {
+    navContent: 'Home',
+    navLinks: '/'
+  },
+
+  {
+    navContent: 'About',
+    navLinks: '/'
+  },
+
+  {
+    navContent: 'Contact Us',
+    navLinks: '/'
+  },
+]
+
+
+
   return (
     <>
 
     <nav id='Navbar'>
       <div className="container">
         <div className="manu_row">
-          <div className="logoImg">
+          <div className="logoImg w-[155px] h-[140px]">
             <img src={logo} alt="" />
           </div>
-          <div className="items"></div>
-          <div className="boxButton"></div>
+          <div className="items">
+            {
+              navItems.map((item, i)=>(
+                <Link key={i} className='text-xl font-normal text-primery font-family' to={item.navLinks}>{item.navContent}</Link>
+              ))
+            }
+          </div>
+          <div className="boxButton ">
+            <button>Get in touch</button>
+          </div>
         </div>
       </div>
     </nav>
