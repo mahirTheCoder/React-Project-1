f356f2cfff8b338bced762d2a2649cdd74bcd713
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-08-19 19:06:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+
+
+
+
+
+
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/LayoutOne.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LayoutOne = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default LayoutOne
